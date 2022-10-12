a82f4977f633b08dd03bc5300829d3e9bcab2c81
nayanakodithuwakku <nayanakodithuwakku2000@gmail.com>
2022-10-12 13:32:31 +0530

    java Project Code

---
+++ src/Home.java
@@ -0,0 +1,22 @@
+class Home {
+    static void text (int x){
+        System.out.println("12345");
+    }
+    static void text (String y){
+        System.out.println("123 abs abc");
+    }
+    static void  text (int z, String a){
+        System.out.println("54abcd");
+    }
+    static void text (double b){
+        System.out.println("3.89");
+    }
+    public static void main (String [] args){
+        text(5);
+        text("hii");
+        text(5,"go");
+        text(5.44);
+
+    }
+
+}
